42c6f4eddccc200f0138558a928a3555ef6c95c3
Rohmat Dasuki <rohmat661@gmail.com>
2024-04-12 21:01:04 +0800

    feat: update changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from module.compare import compare_pdf_content
 from module.download import download_file
 from datetime import datetime,timedelta
@@ -8,11 +9,16 @@ today = datetime.now().date()
 yesterday = today - timedelta(days=1)
 
 url = "https://www.sinarmas-am.co.id/uploads/product/simas-saham-unggulan/simas-saham-unggulan-prospectus.pdf"
-save_path = f"./file/{today}.pdf"
+save_path = f"./file/{today}-simas-saham-unggulan-prospectus.pdf"
 
 download_file(url, save_path)
+# Specify the path to the folder
+folder_path = './file/'
 
-file1_path = f"./file/{yesterday}.pdf"
-file2_path = f"./file/{today}.pdf"
+# Get the list of files in the folder
+files = os.listdir(folder_path)
+
+file1_path = f"./file/{files[0]}"
+file2_path = f"{save_path}"
 
 compare_pdf_content(file1_path, file2_path)
--- module/compare.py
+++ module/compare.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import fitz  # PyMuPDF
 from tkinter import messagebox
 
@@ -13,7 +15,31 @@ def compare_pdf_content(file1_path, file2_path):
     text1 = extract_text_from_pdf(file1_path)
     text2 = extract_text_from_pdf(file2_path)
 
-    if text1 == text2:
+    words1 = set(text1.split())
+    words2 = set(text2.split())
+
+    words1 = set(text1.split())
+    words2 = set(text2.split())
+
+    if words1 != words2:
         messagebox.showinfo("Popup","The content of the PDF files is the same.")
     else:
         messagebox.showinfo("Popup","The content of the PDF files is different.")
+        os.remove(file2_path)
+        copy_file('./file/2024-04-12-simas-saham-unggulan-prospectus copy.pdf','/Users/rohmatdasuki/Documents/')
+     
+def copy_file(source_file, destination_folder):
+    # Check if the source file exists
+    if os.path.exists(source_file):
+        # Check if the destination folder exists, create it if it doesn't
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        # Construct the full path to the destination file
+        destination_file = os.path.join(destination_folder, os.path.basename(source_file))
+
+        # Copy the file to the destination folder
+        shutil.copyfile(source_file, destination_file)
+        print(f"The file '{source_file}' has been successfully copied to '{destination_file}'.")
+    else:
+        print(f"The source file '{source_file}' does not exist.")
